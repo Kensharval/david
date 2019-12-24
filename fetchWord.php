5cf2ca19beb58011a79b0ae9ffe2111302620235
Ibrahim Oladipupo Qamardeen <direct@kensharval.com>
2019-12-24 02:49:11 +0100

    Progress 02:48; Dec 24, 2019

--- fetchWord.php
+++ fetchWord.php
@@ -5,7 +5,22 @@ require_once "processor.php";
 require_once "github.com/xiclonn/php/etp/err/v1/error.php";
 use github_com\xiclonn\php\etp\err as err;
 
-if (! file_exists ("result")) {
-	$respX = processor ()
-	if ($respX != err\Error::Nil ()) {
-		return err\Error ("Dependency failure. ")
+function fetchWord () {
+	if (! file_exists ("result")) {
+		$respX = processor ();
+		if ($respX != err\Error::Nil ()) {
+			return ["", "", err\Error ("Dependency failure; ref: 0.", $respX)];
+		}
+	}
+
+	$size = filesize ("result")
+	if ($size === false) {
+		return ["", "", err\Error ("Dependency failure; ref: 1.")];
+	}
+
+	if ($size == 0) {
+		$respY = processor ();
+		if ($respY != err\Error::Nil ()) {
+			return ["", "", err\Error ("Dependency failure; ref: 2.", $respY)];
+		}
+	}
